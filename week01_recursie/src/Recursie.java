9c2b7dc230130e7e771c804a2ecf9f14e6a147a6
Wannes-Schepers <wannes.schepers@student.ucll.be>
2022-05-25 10:39:05 +0200

    commit

--- week01_recursie/src/Recursie.java
+++ week01_recursie/src/Recursie.java
@@ -7,34 +7,82 @@ public class Recursie {
     // Oefening 1: n-de Fibonacci-getal
 
     public static int fibonacci(int getal) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if(getal <= 0) {
+            throw new IllegalArgumentException();
+        }
+        if(getal <= 2){
+            return 1;
+        }
+        else {
+            return fibonacci(getal - 1) + fibonacci(getal - 2);
+        }
     }
 
     // Oefening 2 : som van cijfers
 
     public static int somVanCijfers(int getal) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        getal = Math.abs(getal);
+        if(getal < 10) {
+            return getal;
+        }
+        else {
+            return getal % 10 + somVanCijfers(getal / 10);
+        }
     }
 
     // Oefening 3: keer een string om
     public static String keerOm(String s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if(s == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if(s.length() <= 1) {
+            return s;
+        }
+        else  {
+            return s.charAt(s.length() - 1) + Recursie.keerOm(s.substring(0, s.length() - 1));
+        }
     }
 
     //oefening 4: countX
 
     public static int countX(String s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if(s == null) {
+            throw new IllegalArgumentException();
+        }
+        if(s.length() == 0) {
+            return 0;
+        }
+        else {
+            return ((s.charAt(0) == 'x') ? 1 : 0) + countX(s.substring(1));
+        }
     }
 
     //oefening 5 : countHi
     public static int countHi(String s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if(s == null) {
+            throw new IllegalArgumentException();
+        }
+        if(s.length() < 2) {
+            return 0;
+        }
+        else {
+            if ((s.substring(0, 2)).equals("hi")) {
+                return 1 + countHi(s.substring(2));
+            } else
+                return countHi(s.substring(1));
+        }
+
     }
 
     // oefening 6
     public static String changeXY(String s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (s == null)
+            throw new IllegalArgumentException();
+        if (s.length() == 0)
+            return s;
+        else
+            return ((s.charAt(0) == 'x' ? "y" : s.charAt(0)) + changeXY(s.substring(1)));
     }
 
     // oefening 7
@@ -58,4 +106,7 @@ public class Recursie {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
+    public static int aantalKaarten(int i) {
+        return 0;
+    }
 }
